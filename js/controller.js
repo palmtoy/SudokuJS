706ce64f233ef2d3b867dacdd2d2ca12c16fc213
DHTMLGoodies <post@dhtmlgoodies.com>
2015-04-27 13:05:23 +0200

    controller

--- js/controller.js
+++ js/controller.js
@@ -1,18 +1,46 @@
 if(!Sudoku)var Sudoku = {};
 
-Sudou.Controller = function(url){
+Sudoku.Controller = function(url){
     if(url != undefined)this.url = url;
 };
 
-$.extends(Sudoku.Controller.prototype, {
-    url : undefined,
+$.extend(Sudoku.Controller.prototype, {
+    url : 'sudoku-controller.php',
 
     load:function(level, index){
 
     },
 
-    loadRandom:function(level){
+    loadRandomBy:function(level){
+        this.request({
+            getRandom:true,
+            level:level
+        });
+    },
 
+    request:function(data){
+        $.ajax({
+            dataType : 'json',
+            method:'post',
+            url: this.url,
+            context : this,
+            data:data
+        }).done(function(response){
+            this.onLoad(response);
+        });
+    },
 
+    loadRandom:function(){
+        this.request({
+            getRandom:true
+        });
+    },
+
+    onLoad:function(data){
+        var model = new Sudoku.Model(data.puzzle);
+        model.setGameId(data.index);
+        model.setLevel(data.level);
+
+        $(this).trigger("loadmodel", model);
     }
 });
--- sudoku-controller.php
+++ sudoku-controller.php
@@ -6,16 +6,13 @@ require_once("php/SudokuReader.php");
 define("SUDOKU_PUZZLE_PATH", "puzzles");
 define("SUDOKU_CACHE_PATH", "sudoku_cache");
 
-
-$sudokuReader = new SudokuReader();
-echo json_encode($sudokuReader->getRandom());
-
 if(isset($_POST['getRandom'])){
 
     if(isset($_POST['level'])){
-
+        $sudokuReader = new SudokuReader();
+        echo json_encode($sudokuReader->getRandomBy($_POST['level']));
     }else{
-
+        $sudokuReader = new SudokuReader();
+        echo json_encode($sudokuReader->getRandom());
     }
-
 }
