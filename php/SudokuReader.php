a707ece4a6f925b531d320eea54a0aa273cc9fd1
DHTMLGoodies <post@dhtmlgoodies.com>
2015-05-02 14:29:50 +0200

    Sudoku Puzzle

--- php/SudokuReader.php
+++ php/SudokuReader.php
@@ -1,11 +1,27 @@
 <?php
+/**
+Author: Alf Magne Kalleland
+Copyright (c) 2015, Cellar Labs AS
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+ */
+
 
 /**
- * Created by PhpStorm.
- * User: Alf Magne
- * Date: 27.04.2015
- * Time: 11:07
+ * The 1600 puzzles in the puzzles folder have a commercial license attached to it. Purchase
+ * a commercial license from dhtmlgoodies.com to use them.
+ *
+ * The 80 puzzles in the puzzles-free folder can be used freely.
  */
+
 class SudokuReader
 {
 
